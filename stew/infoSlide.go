01a077c3c4ce0d1e10e58b417f8c264fa7491c82
postmannen <postmannen@gmail.com>
2021-07-01 07:29:21 +0200

    added generation of info menu, and info page

--- stew/infoSlide.go
+++ stew/infoSlide.go
@@ -1,11 +1,20 @@
 package stew
 
-import "github.com/rivo/tview"
+import (
+	"fmt"
+
+	"github.com/rivo/tview"
+)
 
 func infoSlide(app *tview.Application) tview.Primitive {
 	flex := tview.NewFlex()
 	flex.SetTitle("info")
 	flex.SetBorder(true)
+
+	textView := tview.NewTextView()
+	flex.AddItem(textView, 0, 1, false)
+
+	fmt.Fprintf(textView, "Information page for Stew.\n")
 
 	return flex
 }
--- stew/messageSlide.go
+++ stew/messageSlide.go
@@ -1,6 +1,7 @@
 package stew
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -452,4 +453,141 @@ func messageSlide(app *tview.Application) tview.Primitive {
 	app.SetFocus(p.msgInputForm)
 
 	return p.flex
+}
+
+// Check and compare all the fields of the main message struct
+// used in Steward, and the message struct used in Stew that they are
+// equal.
+// If they are not equal an error will be returned to the user with
+// the name of the field that was missing in the Stew message struct.
+//
+// Some of the fields in the Steward Message struct are used by the
+// system for control, and not needed when creating an initial message
+// template, and we can add case statements for those fields below
+// that we do not wan't to check.
+func compareMsgAndMessage() error {
+	stewardMessage := steward.Message{}
+	stewMsg := msg{}
+
+	stewardRefVal := reflect.ValueOf(stewardMessage)
+	stewRefVal := reflect.ValueOf(stewMsg)
+
+	// Loop trough all the fields of the Message struct.
+	for i := 0; i < stewardRefVal.NumField(); i++ {
+		found := false
+
+		for ii := 0; ii < stewRefVal.NumField(); ii++ {
+			if stewardRefVal.Type().Field(i).Name == stewRefVal.Type().Field(ii).Name {
+				found = true
+				break
+			}
+		}
+
+		// Case statements for the fields we don't care about for
+		// the message template.
+		if !found {
+			switch stewardRefVal.Type().Field(i).Name {
+			case "ID":
+				// Not used in message template.
+			case "FromNode":
+				// Not used in message template.
+			case "PreviousMessage":
+				// Not used in message template.
+			case "done":
+				// Not used in message template.
+			default:
+				return fmt.Errorf("error: %v within the steward Message struct were not found in the stew msg struct", stewardRefVal.Type().Field(i).Name)
+
+			}
+		}
+	}
+
+	return nil
+}
+
+// Will return the Label And the text Value of an input or dropdown form field.
+func getLabelAndValue(fi tview.FormItem) (string, string) {
+	var label string
+	var value string
+	switch v := fi.(type) {
+	case *tview.InputField:
+		value = v.GetText()
+		label = v.GetLabel()
+	case *tview.DropDown:
+		label = v.GetLabel()
+		_, value = v.GetCurrentOption()
+	}
+
+	return label, value
+}
+
+// Check if number is int.
+func validateInteger(text string, ch rune) bool {
+	if text == "-" {
+		return true
+	}
+	_, err := strconv.Atoi(text)
+	return err == nil
+}
+
+// getNodes will load all the node names from a file, and return a slice of
+// string values, each representing a unique node.
+func getNodeNames(filePath string) ([]string, error) {
+
+	fh, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error: unable to open node file: %v", err)
+	}
+	defer fh.Close()
+
+	nodes := []string{}
+
+	scanner := bufio.NewScanner(fh)
+	for scanner.Scan() {
+		node := scanner.Text()
+		nodes = append(nodes, node)
+	}
+
+	return nodes, nil
+}
+
+// -----------------------
+
+// Creating a copy of the real Message struct here to use within the
+// field specification, but without the control kind of fields from
+// the original to avoid changing them to pointer values in the main
+// struct which would be needed when json marshaling to omit those
+// empty fields.
+type msg struct {
+	// The node to send the message to
+	ToNode steward.Node `json:"toNode" yaml:"toNode"`
+	// The actual data in the message
+	Data []string `json:"data" yaml:"data"`
+	// Method, what is this message doing, etc. CLI, syslog, etc.
+	Method steward.Method `json:"method" yaml:"method"`
+	// ReplyMethod, is the method to use for the reply message.
+	// By default the reply method will be set to log to file, but
+	// you can override it setting your own here.
+	ReplyMethod steward.Method `json:"replyMethod" yaml:"replyMethod"`
+	// From what node the message originated
+	ACKTimeout int `json:"ACKTimeout" yaml:"ACKTimeout"`
+	// Resend retries
+	Retries int `json:"retries" yaml:"retries"`
+	// The ACK timeout of the new message created via a request event.
+	ReplyACKTimeout int `json:"replyACKTimeout" yaml:"replyACKTimeout"`
+	// The retries of the new message created via a request event.
+	ReplyRetries int `json:"replyRetries" yaml:"replyRetries"`
+	// Timeout for long a process should be allowed to operate
+	MethodTimeout int `json:"methodTimeout" yaml:"methodTimeout"`
+	// Directory is a string that can be used to create the
+	//directory structure when saving the result of some method.
+	// For example "syslog","metrics", or "metrics/mysensor"
+	// The type is typically used in the handler of a method.
+	Directory string `json:"directory" yaml:"directory"`
+	// FileExtension is used to be able to set a wanted extension
+	// on a file being saved as the result of data being handled
+	// by a method handler.
+	FileExtension string `json:"fileExtension" yaml:"fileExtension"`
+	// operation are used to give an opCmd and opArg's.
+	Operation interface{} `json:"operation,omitempty"`
 }
