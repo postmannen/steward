f32f42d2cedc686ae4d8cac9c0f900adbfe56b2e
postmannen <postmannen@gmail.com>
2021-12-07 16:00:49 +0100

    updated nats-server wrapper

--- scripts/nats-server/wrapper-docker-compose.sh
+++ scripts/nats-server/wrapper-docker-compose.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 # TO USE:
-# ./wrapper ./le.crt ./le.key 50222
+# ./wrapper fqdn 50222
 
-LE_CRT=$1
-LE_KEY=$2
-EXPOSED_NATS_PORT=$3
+FQDN=$1
+
+if docker run -it --rm -v "$PWD:/certs/:rw" -p 80:80 -p 443:443 -e DAEMON=false -e USER_FOLDER=/certs -e PROD=false -e DOMAIN="$FQDN" certupdater:0.1.0; then
+    echo " * successfully generated LetsEncrypt certificate"
+else
+    echo " * failed to generate LetsEncrypt certificate."
+    echo " * a docker image of cert updater need to be present on the host."
+    echo " * Follow the instructions at https://github.com/RaaLabs/certupdater"
+    echo " * to install it as a docker image."
+fi
+
+LE_CRT=$PWD/$FQDN/$FQDN.crt
+LE_KEY=$PWD/$FQDN/$FQDN.key
+EXPOSED_NATS_PORT=$2
 
 if go run ./steward/scripts/nats-server/generate-nkeys/main.go; then
     echo " * succesfully generated nkeys"
